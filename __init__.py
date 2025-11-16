3e28caa13fd41b03ead2b47502392001b9257661
PawWin <winkler.pawel2@gmail.com>
2025-11-16 09:57:37 +0100

    feat: created package for easier import [#86c6hwbk0]

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from .testy_jednostkowe_functions import (
+    is_palindrome,
+    fibonacci,
+    count_vowels,
+    calculate_discount,
+    flatten_list,
+    word_frequency,
+    is_prime,
+)
+
+__all__ = [
+    "is_palindrome",
+    "fibonacci",    
+    "count_vowels",
+    "calculate_discount",
+    "flatten_list",
+    "word_frequency",
+    "is_prime",
+]
